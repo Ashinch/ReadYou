c34e77a82a89d0bd3c8721bb3a49651ca5521725
Ash <Glaxyinfinite@outlook.com>
2023-03-06 21:59:02 +0800

    Fix the "Not valid credentials" issue caused by trailing separator in server URL

--- app/src/main/java/me/ash/reader/ui/page/settings/accounts/addition/AddFeverAccountDialog.kt
+++ app/src/main/java/me/ash/reader/ui/page/settings/accounts/addition/AddFeverAccountDialog.kt
@@ -12,8 +12,11 @@ import androidx.compose.material.icons.rounded.RssFeed
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
@@ -112,7 +115,9 @@ fun AddFeverAccountDialog(
                         type = AccountType.Fever,
                         name = context.getString(R.string.fever),
                         securityKey = FeverSecurityKey(
-                            serverUrl = serverUrl,
+                            serverUrl = serverUrl
+                                .takeIf { !it.endsWith("/") }
+                                ?: serverUrl.trimEnd('/'),
                             username = username,
                             password = password,
                         ).toString(),
