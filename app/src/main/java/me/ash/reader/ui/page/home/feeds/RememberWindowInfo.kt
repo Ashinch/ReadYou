8d21032377b5fbb995e1508c19f3962d3f031476
mosayeb <angashtemosayeb@gmail.com>
2023-12-23 02:43:42 -0800

    support large screens

---
+++ app/src/main/java/me/ash/reader/ui/page/home/feeds/RememberWindowInfo.kt
@@ -0,0 +1,40 @@
+package me.ash.reader.ui.page.home.feeds
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun rememberWindowInfo(): WindowInfo {
+    val configuration = LocalConfiguration.current
+    return WindowInfo(
+        screenWidthInfo = when {
+            configuration.screenWidthDp < 600 -> WindowInfo.WindowType.Compat
+            configuration.screenWidthDp < 840 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        screenHeightInfo = when {
+            configuration.screenWidthDp < 480 -> WindowInfo.WindowType.Compat
+            configuration.screenWidthDp < 900 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        screenWidth = configuration.screenWidthDp.dp,
+        screenHeight = configuration.screenWidthDp.dp,
+    )
+}
+
+data class WindowInfo(
+    val screenWidthInfo: WindowType,
+    val screenHeightInfo: WindowType,
+    val screenWidth: Dp,
+    val screenHeight: Dp
+
+) {
+    sealed class WindowType {
+        object Compat : WindowType()
+        object Medium : WindowType()
+        object Expanded : WindowType()
+    }
+}
