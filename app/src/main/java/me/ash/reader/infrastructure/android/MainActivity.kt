ecc2f793d19e23a91ec68da1c3b5697a2b202a1f
Mark Bestavros <markbest@bu.edu>
2024-01-12 13:12:06 -0500

    Add workaround for "Row too big to fit into CursorWindow" bug
    Signed-off-by: Mark Bestavros <markbest@bu.edu>

--- app/src/main/java/me/ash/reader/infrastructure/android/MainActivity.kt
+++ app/src/main/java/me/ash/reader/infrastructure/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package me.ash.reader.infrastructure.android
 
+import android.database.CursorWindow
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -19,7 +20,9 @@ import me.ash.reader.infrastructure.preference.LanguagesPreference
 import me.ash.reader.infrastructure.preference.SettingsProvider
 import me.ash.reader.ui.ext.languages
 import me.ash.reader.ui.page.common.HomeEntry
+import java.lang.reflect.Field
 import javax.inject.Inject
+
 
 /**
  * The Single-Activity Architecture.
@@ -45,6 +48,15 @@ class MainActivity : ComponentActivity() {
         LanguagesPreference.fromValue(languages).let {
             if (it == LanguagesPreference.UseDeviceLanguages) return@let
             it.setLocale(this)
+        }
+
+        // Workaround for https://github.com/Ashinch/ReadYou/issues/312: increase cursor window size
+        try {
+            val field: Field = CursorWindow::class.java.getDeclaredField("sCursorWindowSize")
+            field.isAccessible = true
+            field.set(null, 100 * 1024 * 1024) // 100MB is the new cursor window size
+        } catch (e: Exception) {
+            Log.e("RLog", "Unable to increase cursor window size: ${e.printStackTrace()}")
         }
 
         setContent {
