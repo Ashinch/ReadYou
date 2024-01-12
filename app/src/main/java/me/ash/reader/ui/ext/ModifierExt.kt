805700f264a2c855a9c59d5a151ba8abf3be87e1
zhusuner <80241679+zhusuner@users.noreply.github.com>
2024-01-12 18:19:11 +0800

    Update ModifierExt.kt

--- app/src/main/java/me/ash/reader/ui/ext/ModifierExt.kt
+++ app/src/main/java/me/ash/reader/ui/ext/ModifierExt.kt
@@ -3,25 +3,45 @@ package me.ash.reader.ui.ext
 import android.annotation.SuppressLint
 import android.view.HapticFeedbackConstants
 import android.view.SoundEffectConstants
+import androidx.compose.animation.core.snap
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.lerp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.PagerScope
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalPagerApi::class)
@@ -126,4 +146,165 @@ fun Modifier.combinedFeedbackClickable(
             },
         )
     }
+}
+
+enum class ArticleSwipeDirection(val raw: Int) {
+    Up(1),
+    Right(2),
+    Down(3),
+    Left(4),
+    Default(0)
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+fun Modifier.swipeLeftAndRight(onLeft: () -> Unit, onRight: () -> Unit): Modifier = composed {
+    var screenWidth by rememberSaveable { mutableStateOf(0f) }
+    var screenHeight by rememberSaveable { mutableStateOf(0f) }
+    val swipeableState = rememberSwipeableState(
+        ArticleSwipeDirection.Default,
+        animationSpec = snap()
+    )
+    val connection = remember {
+        object: NestedScrollConnection {
+            // Let the children eat first, we consume nothing
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset { return Offset.Zero }
+
+            // Let it scroll...
+            override suspend fun onPreFling(available: Velocity): Velocity { return Velocity.Zero }
+
+            // We consume the rest
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource,
+            ): Offset {
+                // use leftover delta to swipe parent
+                return Offset(0f, swipeableState.performDrag(available.x))
+            }
+
+            // We fling but with zero speed (needed to trigger the event, but too fast will overshoot)
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                // perform fling on parent to trigger state change
+                swipeableState.performFling(velocity = 0f)
+                return available
+            }
+        }
+    }
+    val anchorWeight = remember(screenWidth) {
+        if (screenWidth == 0f) {
+            1f
+        } else {
+            screenWidth
+        }
+    }
+    val scope = rememberCoroutineScope()
+    if (swipeableState.isAnimationRunning) {
+        DisposableEffect(Unit) {
+            onDispose {
+                when (swipeableState.currentValue) {
+                    ArticleSwipeDirection.Left -> {
+                        onLeft()
+                    }
+                    ArticleSwipeDirection.Right -> {
+                        onRight()
+                    }
+                    else -> {
+                        return@onDispose
+                    }
+                }
+                scope.launch {
+                    swipeableState.snapTo(ArticleSwipeDirection.Default)
+                }
+            }
+        }
+    }
+    return@composed Modifier
+        .onSizeChanged { screenWidth = it.width.toFloat() }
+        .nestedScroll(connection)
+        .swipeable(
+            state = swipeableState,
+            anchors = mapOf(
+                0f to ArticleSwipeDirection.Right,
+                anchorWeight / 2 to ArticleSwipeDirection.Default,
+                anchorWeight to ArticleSwipeDirection.Left,
+            ),
+            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+            orientation = Orientation.Horizontal,
+        )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+fun Modifier.swipeableUpDown(onUp: () -> Unit, onDown: () -> Unit): Modifier = composed {
+    var screenHeight by rememberSaveable { mutableStateOf(0f) }
+    val swipeableState = rememberSwipeableState(
+        ArticleSwipeDirection.Default,
+        animationSpec = snap()
+    )
+    val connection = remember {
+        object: NestedScrollConnection {
+            // Let the children eat first, we consume nothing
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset { return Offset.Zero }
+
+            // Let it scroll...
+            override suspend fun onPreFling(available: Velocity): Velocity { return Velocity.Zero }
+
+            // We consume the rest
+            override fun onPostScroll(
+                consumed: Offset,
+                available: Offset,
+                source: NestedScrollSource,
+            ): Offset {
+                // use leftover delta to swipe parent
+                return Offset(0f, swipeableState.performDrag(available.y))
+            }
+
+            // We fling but with zero speed (needed to trigger the event, but too fast will overshoot)
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                // perform fling on parent to trigger state change
+                swipeableState.performFling(velocity = 0f)
+                return available
+            }
+        }
+    }
+    val anchorHeight = remember(screenHeight) {
+        if (screenHeight == 0f) {
+            1f
+        } else {
+            screenHeight
+        }
+    }
+    val scope = rememberCoroutineScope()
+    if (swipeableState.isAnimationRunning) {
+        DisposableEffect(Unit) {
+            onDispose {
+                when (swipeableState.currentValue) {
+                    ArticleSwipeDirection.Up -> {
+                        onUp()
+                    }
+                    ArticleSwipeDirection.Down -> {
+                        onDown()
+                    }
+                    else -> {
+                        return@onDispose
+                    }
+                }
+                scope.launch {
+                    swipeableState.snapTo(ArticleSwipeDirection.Default)
+                }
+            }
+        }
+    }
+    return@composed Modifier
+        .onSizeChanged { screenHeight = it.height.toFloat() }
+        .nestedScroll(connection)
+        .swipeable(
+            state = swipeableState,
+            anchors = mapOf(
+                0f to ArticleSwipeDirection.Up,
+                anchorHeight / 2 to ArticleSwipeDirection.Default,
+                anchorHeight to ArticleSwipeDirection.Down,
+            ),
+            thresholds = { _, _ -> FractionalThreshold(0.3f) },
+            orientation = Orientation.Vertical,
+        )
 }
