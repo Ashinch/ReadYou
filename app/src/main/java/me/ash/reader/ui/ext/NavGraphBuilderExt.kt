34e664832311a53fbfa4c7b5c17f4fccfd8e9b6d
junkfood <69683722+JunkFood02@users.noreply.github.com>
2024-01-18 23:31:43 +0800

    feat(ui): use forward and backward transition (#540)

--- app/src/main/java/me/ash/reader/ui/ext/NavGraphBuilderExt.kt
+++ app/src/main/java/me/ash/reader/ui/ext/NavGraphBuilderExt.kt
@@ -16,36 +16,67 @@ import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import com.google.accompanist.navigation.animation.composable
+import me.ash.reader.ui.motion.materialSharedAxisXIn
+import me.ash.reader.ui.motion.materialSharedAxisXOut
 
 @OptIn(ExperimentalAnimationApi::class)
+@Deprecated(message = "Migrate to Forward and backward transition", replaceWith = ReplaceWith("forwardAndBackwardComposable(route = route, arguments = arguments, deepLinks = deepLinks) { content() }")
+)
 fun NavGraphBuilder.animatedComposable(
-    route: String,
-    arguments: List<NamedNavArgument> = emptyList(),
-    deepLinks: List<NavDeepLink> = emptyList(),
-    content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit,
+        route: String,
+        arguments: List<NamedNavArgument> = emptyList(),
+        deepLinks: List<NavDeepLink> = emptyList(),
+        content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit,
 ) = composable(
-    route = route,
-    arguments = arguments,
-    deepLinks = deepLinks,
-    enterTransition = {
-        fadeIn(animationSpec = tween(220, delayMillis = 90)) +
-                scaleIn(
-                    initialScale = 0.92f,
-                    animationSpec = tween(220, delayMillis = 90)
-                )
-    },
-    exitTransition = {
-        fadeOut(animationSpec = tween(90))
-    },
-    popEnterTransition = {
-        fadeIn(animationSpec = tween(220, delayMillis = 90)) +
-                scaleIn(
-                    initialScale = 0.92f,
-                    animationSpec = tween(220, delayMillis = 90)
-                )
-    },
-    popExitTransition = {
-        fadeOut(animationSpec = tween(90))
-    },
-    content = content
+        route = route,
+        arguments = arguments,
+        deepLinks = deepLinks,
+        enterTransition = {
+            fadeIn(animationSpec = tween(220, delayMillis = 90)) +
+                    scaleIn(
+                            initialScale = 0.92f,
+                            animationSpec = tween(220, delayMillis = 90)
+                    )
+        },
+        exitTransition = {
+            fadeOut(animationSpec = tween(90))
+        },
+        popEnterTransition = {
+            fadeIn(animationSpec = tween(220, delayMillis = 90)) +
+                    scaleIn(
+                            initialScale = 0.92f,
+                            animationSpec = tween(220, delayMillis = 90)
+                    )
+        },
+        popExitTransition = {
+            fadeOut(animationSpec = tween(90))
+        },
+        content = content
+)
+
+private const val INITIAL_OFFSET_FACTOR = 0.10f
+
+@OptIn(ExperimentalAnimationApi::class)
+fun NavGraphBuilder.forwardAndBackwardComposable(
+        route: String,
+        arguments: List<NamedNavArgument> = emptyList(),
+        deepLinks: List<NavDeepLink> = emptyList(),
+        content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit
+) = composable(
+        route = route,
+        arguments = arguments,
+        deepLinks = deepLinks,
+        enterTransition = {
+            materialSharedAxisXIn(initialOffsetX = { (it * INITIAL_OFFSET_FACTOR).toInt() })
+        },
+        exitTransition = {
+            materialSharedAxisXOut(targetOffsetX = { -(it * INITIAL_OFFSET_FACTOR).toInt() })
+        },
+        popEnterTransition = {
+            materialSharedAxisXIn(initialOffsetX = { -(it * INITIAL_OFFSET_FACTOR).toInt() })
+        },
+        popExitTransition = {
+            materialSharedAxisXOut(targetOffsetX = { (it * INITIAL_OFFSET_FACTOR).toInt() })
+        },
+        content = content
 )
