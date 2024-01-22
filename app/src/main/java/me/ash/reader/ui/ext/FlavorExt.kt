1b630dd84a0b5eee76e11dd961a1f6ff294499bd
Ash <Glaxyinfinite@outlook.com>
2024-01-22 12:51:05 +0800

    chore(build): add google play variant

--- app/src/main/java/me/ash/reader/ui/ext/FlavorExt.kt
+++ app/src/main/java/me/ash/reader/ui/ext/FlavorExt.kt
@@ -6,6 +6,8 @@ import me.ash.reader.BuildConfig
 
 const val GITHUB = "github"
 const val FDROID = "fdroid"
+const val GOOGLE_PLAY = "googlePlay"
 
-const val isFdroid = BuildConfig.FLAVOR == FDROID
-const val notFdroid = !isFdroid
+const val isFDroid = BuildConfig.FLAVOR == FDROID
+const val isGitHub = BuildConfig.FLAVOR == GITHUB
+const val isGooglePlay = BuildConfig.FLAVOR == GOOGLE_PLAY
