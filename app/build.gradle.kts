8b98dc85245bdedaf947a274a8315992177cd90e
aalbert <augustin.albert@bleu-azure.fr>
2024-06-15 14:27:45 +0200

    sort libraries in several groups, to be updated together

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,17 +111,47 @@ android {
 }
 
 dependencies {
+    // AboutLibraries
     implementation(libs.aboutlibraries.core)
     implementation(libs.aboutlibraries.compose)
+
+    // Compose
     implementation(libs.compose.html)
-    implementation(libs.android.svg)
-    implementation(libs.opml.parser)
-    implementation(libs.readability4j)
-    implementation(libs.rome)
+    implementation(platform(libs.compose.bom))
+    androidTestImplementation(platform(libs.compose.bom))
+    implementation(libs.compose.animation.graphics)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.util)
+    implementation(libs.compose.material)
+    implementation(libs.compose.material.icons.extended)
+    implementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.tooling.preview)
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    implementation(libs.compose.material3)
+
+    // Accompanist
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.flowlayout)
+    implementation(libs.accompanist.swiperefresh)
+
+    // Coil
     implementation(libs.coil.base)
     implementation(libs.coil.compose)
     implementation(libs.coil.svg)
     implementation(libs.coil.gif)
+
+    // Hilt
+    implementation(libs.hilt.work)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    // AndroidX
+    implementation(libs.android.svg)
+    implementation(libs.opml.parser)
+    implementation(libs.readability4j)
+    implementation(libs.rome)
     implementation(libs.telephoto)
     implementation(libs.okhttp)
     implementation(libs.okhttp.coroutines)
@@ -141,28 +171,11 @@ dependencies {
     implementation(libs.navigation.compose)
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.ktx)
-    implementation(libs.compose.material3)
-    implementation(libs.accompanist.pager)
-    implementation(libs.accompanist.flowlayout)
-    implementation(libs.accompanist.swiperefresh)
-    implementation(platform(libs.compose.bom))
-    androidTestImplementation(platform(libs.compose.bom))
-    implementation(libs.compose.animation.graphics)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.util)
-    implementation(libs.compose.material)
-    implementation(libs.compose.material.icons.extended)
-    implementation(libs.compose.ui.tooling)
-    implementation(libs.compose.ui.tooling.preview)
-    androidTestImplementation(libs.compose.ui.test.junit4)
-    implementation(libs.hilt.work)
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-    ksp(libs.hilt.compiler)
-    implementation(libs.hilt.navigation.compose)
     implementation(libs.core.ktx)
     implementation(libs.activity.compose)
     implementation(libs.appcompat)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.junit.ext)
     androidTestImplementation(libs.espresso)
