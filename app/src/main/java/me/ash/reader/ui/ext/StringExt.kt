0584290e506801123fe68444472ac0933320fe5e
Ash <Ashinch@outlook.it>
2024-06-26 16:05:20 +0800

    test(opml): empty title (#782)

--- app/src/main/java/me/ash/reader/ui/ext/StringExt.kt
+++ app/src/main/java/me/ash/reader/ui/ext/StringExt.kt
@@ -51,13 +51,13 @@ fun String?.orNotEmpty(l: (value: String) -> String): String =
 
 fun String.requiresBidi(): Boolean = Bidi.requiresBidi(this.toCharArray(), 0, this.length)
 
-fun String?.extractDomain(): String {
-    if (this.isNullOrBlank()) return ""
+fun String?.extractDomain(): String? {
+    if (this.isNullOrBlank()) return null
     val urlMatchResult = Regex("(?<=://)([\\w\\d.-]+)").find(this)
     if (urlMatchResult != null) {
         return urlMatchResult.value
     }
     val domainRegex = Regex("[\\w\\d.-]+\\.[\\w\\d.-]+")
     val domainMatchResult = domainRegex.find(this)
-    return domainMatchResult?.value ?: ""
+    return domainMatchResult?.value
 }
