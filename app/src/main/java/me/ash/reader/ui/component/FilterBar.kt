fce24968db46bb3e311312b6eff6ccf676935142
junkfood <69683722+JunkFood02@users.noreply.github.com>
2024-11-12 16:28:12 +0800

    feat(ui): short navigation bar

--- app/src/main/java/me/ash/reader/ui/component/FilterBar.kt
+++ app/src/main/java/me/ash/reader/ui/component/FilterBar.kt
@@ -3,11 +3,19 @@ package me.ash.reader.ui.component
 import android.os.Build
 import android.view.SoundEffectConstants
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalView
@@ -22,6 +30,7 @@ import me.ash.reader.ui.theme.palette.onDark
 
 @Composable
 fun FilterBar(
+    modifier: Modifier = Modifier,
     filter: Filter,
     filterBarStyle: Int,
     filterBarFilled: Boolean,
@@ -37,58 +46,69 @@ fun FilterBar(
         MaterialTheme.colorScheme.primaryContainer
     } onDark MaterialTheme.colorScheme.secondaryContainer
 
-    NavigationBar(
-        modifier = Modifier,
-        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(filterBarTonalElevation),
-        tonalElevation = 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.surfaceColorAtElevation(filterBarTonalElevation),
+        modifier = modifier
     ) {
-        Spacer(modifier = Modifier.width(filterBarPadding))
-        Filter.values.forEach { item ->
-            NavigationBarItem(
-//                        modifier = Modifier.height(60.dp),
-                alwaysShowLabel = when (filterBarStyle) {
-                    FlowFilterBarStylePreference.Icon.value -> false
-                    FlowFilterBarStylePreference.IconLabel.value -> true
-                    FlowFilterBarStylePreference.IconLabelOnlySelected.value -> false
-                    else -> false
-                },
-                icon = {
-                    Icon(
-                        imageVector = if (filter == item && filterBarFilled) {
-                            item.iconFilled
-                        } else {
-                            item.iconOutline
-                        },
-                        contentDescription = item.toName()
-                    )
-                },
-                label = if (filterBarStyle == FlowFilterBarStylePreference.Icon.value) {
-                    null
-                } else {
-                    {
-                        Text(
-                            text = item.toName(),
-//                            style = MaterialTheme.typography.labelLarge,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
+        Row(
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(NavigationBarDefaults.windowInsets)
+                .defaultMinSize(minHeight = 64.dp)
+                .selectableGroup(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+
+            Spacer(modifier = Modifier.width(filterBarPadding))
+            Filter.values.forEach { item ->
+                NavigationBarItem(
+                    modifier = Modifier.height(64.dp),
+                    alwaysShowLabel = when (filterBarStyle) {
+                        FlowFilterBarStylePreference.Icon.value -> false
+                        FlowFilterBarStylePreference.IconLabel.value -> true
+                        FlowFilterBarStylePreference.IconLabelOnlySelected.value -> false
+                        else -> false
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = if (filter == item && filterBarFilled) {
+                                item.iconFilled
+                            } else {
+                                item.iconOutline
+                            },
+                            contentDescription = item.toName()
                         )
-                    }
-                },
-                selected = filter == item,
-                onClick = {
+                    },
+                    label = if (filterBarStyle == FlowFilterBarStylePreference.Icon.value) {
+                        null
+                    } else {
+                        {
+                            Text(
+                                text = item.toName(),
+//                            style = MaterialTheme.typography.labelLarge,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                            )
+                        }
+                    },
+                    selected = filter == item,
+                    onClick = {
 //                        view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
-                    view.playSoundEffect(SoundEffectConstants.CLICK)
-                    filterOnClick(item)
-                },
-                colors = NavigationBarItemDefaults.colors(
-                    indicatorColor = indicatorColor,
-                    unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                    selectedIconColor = MaterialTheme.colorScheme.contentColorFor(indicatorColor),
-                    unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
-                    selectedTextColor = MaterialTheme.colorScheme.onSurface
+                        view.playSoundEffect(SoundEffectConstants.CLICK)
+                        filterOnClick(item)
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = indicatorColor,
+                        unselectedIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        selectedIconColor = MaterialTheme.colorScheme.contentColorFor(indicatorColor),
+                        unselectedTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        selectedTextColor = MaterialTheme.colorScheme.onSurface
+                    )
                 )
-            )
+            }
+            Spacer(modifier = Modifier.width(filterBarPadding))
         }
-        Spacer(modifier = Modifier.width(filterBarPadding))
     }
 }
