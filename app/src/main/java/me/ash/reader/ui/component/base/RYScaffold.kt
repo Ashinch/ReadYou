3ab503e2b8277b8f34db0c1de55edee57e74d85e
Will McCall <532b81611e7e@mail.fordibben.zone>
2024-11-13 18:34:44 +0000

    Make RYScaffold fill max size

--- app/src/main/java/me/ash/reader/ui/component/base/RYScaffold.kt
+++ app/src/main/java/me/ash/reader/ui/component/base/RYScaffold.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -38,6 +39,7 @@ fun RYScaffold(
 
     Scaffold(
         modifier = modifier
+            .fillMaxSize()
             .background(
                 MaterialTheme.colorScheme.surfaceColorAtElevation(
                     topBarTonalElevation,
