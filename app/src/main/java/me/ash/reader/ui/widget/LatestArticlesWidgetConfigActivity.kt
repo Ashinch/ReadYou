c9940cdb9a4c713dada56318c843ffbbd4913832
bunburya <dev@bunburya.eu>
2025-03-13 00:31:59 +0000

    Making widget config follow app theme settings

--- app/src/main/java/me/ash/reader/ui/widget/LatestArticlesWidgetConfigActivity.kt
+++ app/src/main/java/me/ash/reader/ui/widget/LatestArticlesWidgetConfigActivity.kt
@@ -7,14 +7,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.tooling.preview.Preview
+import me.ash.reader.infrastructure.preference.LocalDarkTheme
 import me.ash.reader.infrastructure.preference.widget.WidgetPreferencesManager
-import me.ash.reader.ui.widget.ui.theme.ReadYouTheme
+import me.ash.reader.ui.theme.AppTheme
 
 class LatestArticlesWidgetConfigActivity : ComponentActivity() {
 
@@ -45,33 +42,16 @@ class LatestArticlesWidgetConfigActivity : ComponentActivity() {
                 widgetPreferencesManager.primaryColor.put(appWidgetId, primaryColor)
                 widgetPreferencesManager.onPrimaryColor.put(appWidgetId, onPrimaryColor)
             }
-
-            LatestArticlesWidgetConfigScreen(
-                appWidgetId = appWidgetId,
-                widgetPreferencesManager = widgetPreferencesManager
-            ) {
-                val resultValue = Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-                setResult(RESULT_OK, resultValue)
-                finish()
+            AppTheme(useDarkTheme = LocalDarkTheme.current.isDarkTheme()) {
+                LatestArticlesWidgetConfigScreen(
+                    appWidgetId = appWidgetId,
+                    widgetPreferencesManager = widgetPreferencesManager
+                ) {
+                    val resultValue = Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                    setResult(RESULT_OK, resultValue)
+                    finish()
+                }
             }
         }
-    }
-}
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReadYouTheme {
-        Greeting("Android")
     }
 }
