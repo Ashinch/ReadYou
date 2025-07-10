3fb9b72601bf48dede478394b5706b8f9589ba48
junkfood <69683722+JunkFood02@users.noreply.github.com>
2025-07-11 01:39:35 +0800

    fix: rename & edit url dialog initial state

--- app/src/main/java/me/ash/reader/ui/component/ChangeUrlDialog.kt
+++ app/src/main/java/me/ash/reader/ui/component/ChangeUrlDialog.kt
@@ -1,8 +1,11 @@
 package me.ash.reader.ui.component
 
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.res.stringResource
 import me.ash.reader.R
 import me.ash.reader.ui.component.base.TextFieldDialog
@@ -15,14 +18,18 @@ fun ChangeUrlDialog(
     onDismissRequest: () -> Unit = {},
     onConfirm: (String) -> Unit = {},
 ) {
-    TextFieldDialog(
-        visible = visible,
-        title = stringResource(R.string.change_url),
-        icon = Icons.Outlined.Edit,
-        value = value,
-        placeholder = stringResource(R.string.feed_url_placeholder),
-        onValueChange = onValueChange,
-        onDismissRequest = onDismissRequest,
-        onConfirm = onConfirm,
-    )
+    if (visible){
+        val textFieldState = rememberTextFieldState(value)
+        LaunchedEffect(textFieldState) {
+            snapshotFlow { textFieldState.text }.collect { onValueChange(it.toString()) }
+        }
+        TextFieldDialog(
+            textFieldState = textFieldState,
+            title = stringResource(R.string.change_url),
+            icon = Icons.Outlined.Edit,
+            placeholder = stringResource(R.string.feed_url_placeholder),
+            onDismissRequest = onDismissRequest,
+            onConfirm = onConfirm,
+        )
+    }
 }
