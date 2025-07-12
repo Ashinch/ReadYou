290e338716dbcc89e8b5a441c47b831cc822d755
junkfood <69683722+JunkFood02@users.noreply.github.com>
2025-07-13 04:15:38 +0800

    feat: add favicons & subtext to notifications

--- app/src/main/java/me/ash/reader/infrastructure/android/NotificationHelper.kt
+++ app/src/main/java/me/ash/reader/infrastructure/android/NotificationHelper.kt
@@ -1,25 +1,38 @@
 package me.ash.reader.infrastructure.android
 
-import android.Manifest
-import android.app.*
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.graphics.drawable.toBitmapOrNull
+import coil.imageLoader
+import coil.request.ImageRequest
 import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.Random
+import javax.inject.Inject
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import me.ash.reader.R
+import me.ash.reader.domain.model.article.Article
+import me.ash.reader.domain.model.feed.Feed
 import me.ash.reader.domain.model.feed.FeedWithArticle
+import me.ash.reader.infrastructure.di.ApplicationScope
+import me.ash.reader.infrastructure.di.IODispatcher
 import me.ash.reader.ui.page.common.ExtraName
 import me.ash.reader.ui.page.common.NotificationGroupName
-import java.util.*
-import javax.inject.Inject
+import timber.log.Timber
 
-class NotificationHelper @Inject constructor(
-    @ApplicationContext
-    private val context: Context,
+class NotificationHelper
+@Inject
+constructor(
+    @ApplicationContext private val context: Context,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher,
+    @ApplicationScope private val coroutineScope: CoroutineScope,
 ) {
 
     private val notificationManager: NotificationManagerCompat =
@@ -28,87 +41,75 @@ class NotificationHelper @Inject constructor(
                 NotificationChannel(
                     NotificationGroupName.ARTICLE_UPDATE,
                     NotificationGroupName.ARTICLE_UPDATE,
-                    NotificationManager.IMPORTANCE_DEFAULT
+                    NotificationManager.IMPORTANCE_DEFAULT,
                 )
             )
         }
+
+    fun notify(feed: Feed, articles: List<Article>) {
+        if (!notificationManager.areNotificationsEnabled()) return
+        coroutineScope.launch {
+            Timber.d("notify ${feed.name} for ${articles.size} articles")
+
+            val favIcon =
+                withContext(ioDispatcher) {
+                    feed.icon?.let { icon ->
+                        context.imageLoader
+                            .execute(ImageRequest.Builder(context).data(icon).build())
+                            .drawable
+                            ?.toBitmapOrNull()
+                    }
+                }
+
+            notificationManager.notify(
+                feed.id.hashCode(),
+                NotificationCompat.Builder(context, NotificationGroupName.ARTICLE_UPDATE)
+                    .setContentTitle(feed.name)
+                    .setContentText(
+                        context.resources.getQuantityText(
+                            R.plurals.unread_desc,
+                            articles.size,
+                        )
+                    )
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setStyle(NotificationCompat.InboxStyle().setSummaryText(feed.name))
+                    .setGroup(feed.id)
+                    .setGroupSummary(true)
+                    .build(),
+            )
+
+            articles.forEachIndexed { _, article ->
+                val builder =
+                    NotificationCompat.Builder(context, NotificationGroupName.ARTICLE_UPDATE)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setSubText(feed.name)
+                        .setAutoCancel(true)
+                        .setContentTitle(article.title)
+                        .setContentText(article.shortDescription)
+                        .setLargeIcon(favIcon)
+                        .setContentIntent(
+                            PendingIntent.getActivity(
+                                context,
+                                Random().nextInt() + article.id.hashCode(),
+                                Intent(context, MainActivity::class.java).apply {
+                                    flags =
+                                        Intent.FLAG_ACTIVITY_NEW_TASK or
+                                            Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    putExtra(ExtraName.ARTICLE_ID, article.id)
+                                },
+                                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
+                            )
+                        )
+                        .setGroup(feed.id)
+                notificationManager.notify(
+                    Random().nextInt() + article.id.hashCode(),
+                    builder.build(),
+                )
+            }
+        }
+    }
 
     fun notify(feedWithArticle: FeedWithArticle) {
-        notificationManager.createNotificationChannelGroup(
-            NotificationChannelGroup(
-                feedWithArticle.feed.id,
-                feedWithArticle.feed.name
-            )
-        )
-        feedWithArticle.articles.forEach { article ->
-            val builder = NotificationCompat.Builder(context, NotificationGroupName.ARTICLE_UPDATE)
-                .setSmallIcon(R.drawable.ic_notification)
-                .setLargeIcon(
-                    (BitmapFactory.decodeResource(
-                        context.resources,
-                        R.drawable.ic_notification
-                    ))
-                )
-                .setContentTitle(article.title)
-                .setContentIntent(
-                    PendingIntent.getActivity(
-                        context,
-                        Random().nextInt() + article.id.hashCode(),
-                        Intent(context, MainActivity::class.java).apply {
-                            flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                                    Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            putExtra(
-                                ExtraName.ARTICLE_ID,
-                                article.id
-                            )
-                        },
-                        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-                    )
-                )
-                .setGroup(feedWithArticle.feed.id)
-                .setStyle(
-                    NotificationCompat.BigTextStyle()
-                        .bigText(article.shortDescription)
-                        .setSummaryText(feedWithArticle.feed.name)
-                )
-
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            notificationManager.notify(
-                Random().nextInt() + article.id.hashCode(),
-                builder.build().apply {
-                    flags = Notification.FLAG_AUTO_CANCEL
-                }
-            )
-        }
-
-        if (feedWithArticle.articles.size > 1) {
-            notificationManager.notify(
-                Random().nextInt() + feedWithArticle.feed.id.hashCode(),
-                NotificationCompat.Builder(context, NotificationGroupName.ARTICLE_UPDATE)
-                    .setSmallIcon(R.drawable.ic_notification)
-                    .setLargeIcon(
-                        (BitmapFactory.decodeResource(
-                            context.resources,
-                            R.drawable.ic_notification
-                        ))
-                    )
-                    .setStyle(
-                        NotificationCompat.InboxStyle()
-                            .setSummaryText(feedWithArticle.feed.name)
-                    )
-                    .setGroup(feedWithArticle.feed.id)
-                    .setGroupSummary(true)
-                    .build()
-            )
-        }
+        notify(feedWithArticle.feed, feedWithArticle.articles)
     }
 }
