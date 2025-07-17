68e9a47e717d20e38ee528887433b53cde304a73
junkfood <69683722+JunkFood02@users.noreply.github.com>
2025-07-17 22:11:30 +0800

    fix: widget style update

--- app/src/main/java/me/ash/reader/ui/widget/ArticleCardWidget.kt
+++ app/src/main/java/me/ash/reader/ui/widget/ArticleCardWidget.kt
@@ -3,10 +3,8 @@ package me.ash.reader.ui.widget
 import android.annotation.SuppressLint
 import android.content.Context
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.drawable.toBitmapOrNull
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
@@ -33,17 +31,16 @@ import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
-import coil.transform.CircleCropTransformation
 import coil.imageLoader
 import coil.request.ImageRequest
 import coil.size.Dimension
-import coil.size.Scale
 import dagger.hilt.android.EntryPointAccessors
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import me.ash.reader.R
 import me.ash.reader.infrastructure.android.MainActivity
 import me.ash.reader.ui.page.common.ExtraName
+import timber.log.Timber
 
 class ArticleCardWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = ArticleCardWidget()
@@ -54,48 +51,58 @@ class ArticleCardWidget : GlanceAppWidget() {
 
     @SuppressLint("RestrictedApi")
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        val article = withContext(Dispatchers.IO) {
-            val entryPoint =
-                EntryPointAccessors.fromApplication(context, WidgetEntryPoint::class.java)
-            val accountService = entryPoint.accountService()
-            val articleDao = entryPoint.articleDao()
-            val currentAccountId = accountService.getCurrentAccountId()
-            articleDao.queryLatestUnreadArticles(accountId = currentAccountId, limit = 10)
-                .firstOrNull { !it.article.img.isNullOrEmpty() }
-        } ?: return
-        val bitmap = withContext(Dispatchers.IO) {
-            val link = article.article.img
-            val imageLoader = context.imageLoader
-            imageLoader.execute(
-                ImageRequest.Builder(context).data(link)
-                    .size(width = Dimension.Pixels(600), height = Dimension.Undefined)
-                    .build()
-            ).drawable!!.toBitmapOrNull()!!
-        }
+        val article =
+            withContext(Dispatchers.IO) {
+                val entryPoint =
+                    EntryPointAccessors.fromApplication(context, WidgetEntryPoint::class.java)
+                val accountService = entryPoint.accountService()
+                val articleDao = entryPoint.articleDao()
+                val currentAccountId = accountService.getCurrentAccountId()
+                articleDao.queryLatestUnreadArticles(accountId = currentAccountId, limit = 10).let {
+                    it.firstOrNull { !it.article.img.isNullOrEmpty() } ?: it.firstOrNull()
+                }
+            } ?: return
+
+        Timber.d(article.toString())
+        val bitmap =
+            withContext(Dispatchers.IO) {
+                val link = article.article.img
+                val imageLoader = context.imageLoader
+                imageLoader
+                    .execute(
+                        ImageRequest.Builder(context)
+                            .data(link)
+                            .size(width = Dimension.Pixels(600), height = Dimension.Undefined)
+                            .build()
+                    )
+                    .drawable
+                    ?.toBitmapOrNull()
+            }
 
         provideContent {
             GlanceTheme {
-                val backgroundColor = GlanceTheme.colors.surface.getColor(context)
-                val onBackgroundColor = GlanceTheme.colors.onBackground.getColor(context)
+                val titleColor =
+                    if (bitmap != null) Color.White
+                    else GlanceTheme.colors.onSurface.getColor(context)
 
-                val textColor =
-                    if (backgroundColor.luminance() > 0.5f) {
-                        Color.White
-                    } else onBackgroundColor
+                val feedColor =
+                    if (bitmap != null) Color.White
+                    else GlanceTheme.colors.primary.getColor(context)
 
                 // create your AppWidget here
                 WidgetContainer {
                     Box(
                         contentAlignment = Alignment.BottomStart,
-                        modifier = GlanceModifier.fillMaxSize()
-                            .clickable(
-                                actionStartActivity<MainActivity>(
-                                    actionParametersOf(
-                                        ActionParameters.Key<String>(ExtraName.ARTICLE_ID) to article.article.id
+                        modifier =
+                            GlanceModifier.fillMaxSize()
+                                .clickable(
+                                    actionStartActivity<MainActivity>(
+                                        actionParametersOf(
+                                            ActionParameters.Key<String>(ExtraName.ARTICLE_ID) to
+                                                article.article.id
+                                        )
                                     )
-
-                                )
-                            ),
+                                ),
                     ) {
                         //                        Image(
                         //                            provider = ImageProvider(resId =
@@ -105,20 +112,30 @@ class ArticleCardWidget : GlanceAppWidget() {
                         //                            contentScale = ContentScale.Crop,
                         //                            modifier = GlanceModifier.fillMaxSize(),
                         //                        )
-                        Spacer(
-                            modifier =
-                                GlanceModifier.fillMaxSize()
-                                    .background(
-                                        imageProvider = ImageProvider(bitmap),
-                                        contentScale = ContentScale.Crop
-                                    )
-                        )
+                        if (bitmap != null) {
+                            Spacer(
+                                modifier =
+                                    GlanceModifier.fillMaxSize()
+                                        .background(
+                                            imageProvider = ImageProvider(bitmap),
+                                            contentScale = ContentScale.Crop,
+                                        )
+                            )
+                            Spacer(
+                                modifier =
+                                    GlanceModifier.fillMaxSize()
+                                        .background(
+                                            imageProvider = ImageProvider(R.drawable.scrim_gradient)
+                                        )
+                            )
+                        }
+
                         Column(GlanceModifier.padding(12.dp)) {
                             Text(
                                 article.feed.name,
                                 style =
                                     TextStyle(
-                                        color = ColorProvider(textColor),
+                                        color = ColorProvider(feedColor),
                                         fontFamily = FontFamily.SansSerif,
                                         fontSize = 13.sp,
                                         fontWeight = FontWeight.Bold,
@@ -129,7 +146,7 @@ class ArticleCardWidget : GlanceAppWidget() {
                                 article.article.title,
                                 style =
                                     TextStyle(
-                                        color = ColorProvider(textColor),
+                                        color = ColorProvider(titleColor),
                                         fontFamily = FontFamily.Serif,
                                         fontSize = 16.sp,
                                         fontWeight = FontWeight.Bold,
@@ -142,4 +159,3 @@ class ArticleCardWidget : GlanceAppWidget() {
         }
     }
 }
-
