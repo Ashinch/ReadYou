0e8c9f2168fbb57ab098829ad9819452b4b0d153
junkfood <69683722+JunkFood02@users.noreply.github.com>
2025-07-19 19:24:07 +0800

    fix: apply horizontal padding on scaffold content

--- app/src/main/java/me/ash/reader/ui/component/base/RYScaffold.kt
+++ app/src/main/java/me/ash/reader/ui/component/base/RYScaffold.kt
@@ -4,17 +4,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import me.ash.reader.ui.ext.surfaceColorAtElevation
 import me.ash.reader.ui.theme.palette.onDark
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Deprecated("Use m3 Scaffold instead")
 @Composable
 fun RYScaffold(
     modifier: Modifier = Modifier,
@@ -30,39 +35,50 @@ fun RYScaffold(
     content: @Composable () -> Unit = {},
 ) {
     Scaffold(
-        modifier = modifier
-            .background(
+        modifier =
+            modifier.background(
                 MaterialTheme.colorScheme.surfaceColorAtElevation(
                     topBarTonalElevation,
-                    color = containerColor
+                    color = containerColor,
                 )
             ),
-        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
-            containerTonalElevation,
-            color = containerColor
-        ) onDark MaterialTheme.colorScheme.surface,
+        containerColor =
+            MaterialTheme.colorScheme.surfaceColorAtElevation(
+                containerTonalElevation,
+                color = containerColor,
+            ) onDark MaterialTheme.colorScheme.surface,
         topBar = {
-            if (topBar != null) topBar() else if (navigationIcon != null || actions != null) {
+            if (topBar != null) topBar()
+            else if (navigationIcon != null || actions != null) {
                 TopAppBar(
                     title = {},
                     navigationIcon = { navigationIcon?.invoke() },
                     actions = { actions?.invoke(this) },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
-                            topBarTonalElevation
+                    colors =
+                        TopAppBarDefaults.topAppBarColors(
+                            containerColor =
+                                MaterialTheme.colorScheme.surfaceColorAtElevation(
+                                    topBarTonalElevation
+                                )
                         ),
-                    )
                 )
             }
         },
         content = {
-            Column {
+            val layoutDirection = LocalLayoutDirection.current
+            Column(
+                modifier =
+                    Modifier.padding(
+                        start = it.calculateStartPadding(layoutDirection),
+                        end = it.calculateEndPadding(layoutDirection),
+                    )
+            ) {
                 Spacer(modifier = Modifier.height(it.calculateTopPadding()))
                 content()
             }
         },
         bottomBar = { bottomBar?.invoke() },
         floatingActionButton = { floatingActionButton?.invoke() },
-        floatingActionButtonPosition = floatingActionButtonPosition
+        floatingActionButtonPosition = floatingActionButtonPosition,
     )
 }
